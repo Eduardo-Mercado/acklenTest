f2449a4ec366696625dc95f2988b2c6e7c8963b7
Eduardo-Mercado <mercadohedward@gmail.com>
2021-02-12 18:51:32 +0000

    Set up.

---
+++ developer/productionTests/TestPass.cs
@@ -0,0 +1,16 @@
+using Xunit;
+using System;
+using production;
+
+namespace productionTests
+{
+    [Fact]
+    public void TestName()
+    {
+    //Given
+    
+    //When
+    
+    //Then
+    }
+}
---
+++ developer/productionTests/testFail.cs
@@ -0,0 +1,19 @@
+using Xunit;
+using System;
+using production;
+
+namespace productionTests
+{
+    public class TestFail
+    {
+        [Fact]
+        public void TestName()
+        {
+        //Given
+        
+        //When
+        
+        //Then
+        }
+    }
+}
